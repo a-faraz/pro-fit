609c4c190fb4298394945a038969371e21244ee8
Anjum Faraz <anjumfaraz10@gmail.com>
2018-02-21 22:42:59 -0600

    adds reactive store to handle dataflow from firebase

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,17 +1,45 @@
-import { Component } from '@angular/core';
+// Angular
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+// SERVICES
+import { AuthService, User } from '../../../auth/shared/services/auth/auth.service';
+
+// RXJS
+import { Store } from 'store';
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
   template: `
     <div>
-      Pro Fitness App!
+      <h1>{{ user$ | async | json }}</h1>
       <div class="wrapper">
       <router-outlet></router-outlet>
       </div>
     </div>
   `
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+
+  user$: Observable<User>;
+  subscription: Subscription;
+
+  constructor(
+    private _store$: Store,
+    private _authService: AuthService
+  ) {}
+
+  ngOnInit () {
+    // Initiate dataflow for the subscription
+    this.subscription = this._authService.auth$.subscribe();
+    // Set the user from the value in the store
+    this.user$ = this._store$.select<User>('user');
+  }
+
+  ngOnDestroy () {
+    this.subscription.unsubscribe();
+  }
 }
--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,9 +1,36 @@
+import 'rxjs/add/operator/do';
+
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
+import { Store } from 'store'
+
+export interface User {
+  email: string,
+  uid: string,
+  authenticated: boolean
+}
 
 @Injectable()
 export class AuthService {
+
+  // Observable to create new stream
+  auth$ = this.af.authState
+    // Do Operator as a side effect to create user object
+    .do(next => {
+      if (!next) {
+        this._store$.set('user', null);
+        return;
+      }
+      const user: User = {
+        email: next.email,
+        uid: next.uid,
+        authenticated: true
+      };
+      this._store$.set('user', user);
+    });
+
   constructor(
+    private _store$: Store,
     private af: AngularFireAuth
   ) {}
 
